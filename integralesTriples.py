518239d6257e291bfa9d9b096a29b52e11113457
JeffersonBonilla <bm0749042022@unab.edu.sv>
2025-10-03 18:30:53 -0600

    Update integralesTriples.py

--- integralesTriples.py
+++ integralesTriples.py
@@ -79,7 +79,8 @@ def integral():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
